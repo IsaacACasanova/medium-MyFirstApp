65bf273521c30c3dbeb28e91b1390428f2b73d20
IsaacACasanova <Isaac.Casanova@uconn.edu>
2017-01-07 18:04:32 -0500

    MyFirstApp tutorial

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Isaac  Casanova on 1/2/17.
+//  Copyright © 2017 Isaac  Casanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func helloWorldAction(_ sender: Any) {
+        print("Hello, World!")
+    }
+
+}
+
